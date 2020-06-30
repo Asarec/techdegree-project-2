2dd39def5a7aaef9065c2f8ba41558d1421cd2ad
Alfredo Garcia <Asarec@users.noreply.github.com>
2020-06-30 18:10:40 -0400

    Updated function and displayed search input.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+/** Selected Queries */
+const pageHeaderDiv = document.querySelector('.page-header');
+
+/** Created Elements */
+const studentSearchForm = createElement({element: 'form', classSelector: 'student-search'});
+const inputSearch = createElement({element: 'input', attribute: 'placeholder', attributeValue: 'Search for students...'});
+const searchButton = createElement({element: 'button', attribute: 'type', attributeValue: 'submit', content: 'Search'});
+
+/** Appending Childs */
+pageHeaderDiv.appendChild(studentSearchForm);
+studentSearchForm.appendChild(inputSearch);
+studentSearchForm.appendChild(searchButton);
+
 /**
  * Creates an element with the given parameters and returns it in HTML format.
  * @param {string} param.element - HTML Element
@@ -6,15 +19,13 @@
  * @param {string} param.attributeValue - HTML Attribute Value
  * @return {HTMLElement}
  */
-function createElement({element, classSelector, attribute, attributeValue}) {
+function createElement({element, classSelector, attribute, attributeValue, content}) {
     const newElement = document.createElement(element);
 
     // Checks parameters for additional HTML attributes.
-    if (classSelector) newElement.className = classSelector;
-    if (attribute) newElement.setAttribute(attribute, attributeValue);
+    if ( classSelector ) newElement.className = classSelector;
+    if ( attribute ) newElement.setAttribute(attribute, attributeValue);
+    if ( content ) newElement.textContent = content;
 
     return newElement;
 }
-
-// Test Function
-console.log(createElement({element: 'div', classSelector: 'class-name'}));
