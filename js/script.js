d1987ce602ab996ade15bca782d4f38171885ddd
Alfredo Garcia <Asarec@users.noreply.github.com>
2020-06-30 20:13:35 -0400

    switch statement for search function.
    Currently looking for a logical approach for students with middle names.

--- js/script.js
+++ js/script.js
@@ -11,6 +11,47 @@ pageHeaderDiv.appendChild(studentSearchForm);
 studentSearchForm.appendChild(inputSearch);
 studentSearchForm.appendChild(searchButton);
 
+// Search Event
+studentSearchForm.addEventListener ('submit', (event) => {
+    const inputValue = inputSearch.value;
+    const studentList = document.querySelectorAll('.student-item');
+
+    // Prevent the browser from refreshing on submit.
+    event.preventDefault();
+
+    // Clear search bar after submit.
+    inputSearch.value = '';
+
+    for ( let i = 0; i < studentList.length; i++ ) {
+        let studentName = studentList[i].children[0].children[1].textContent.split(' ');
+
+        if ( studentName.length === 3 ) {
+            studentName.splice(1, 1);
+        }
+
+        switch ( inputValue.toUpperCase() ) {
+            case ( studentList[i].children[0].children[1].textContent.toUpperCase() ):
+            case ( studentName[0].toUpperCase() ):
+            case ( studentName[1].toUpperCase() ):
+                studentList[i].style.display = 'list-item';
+                studentList[i].style.border = 'none';
+                studentList[i].style.margin = '0';
+                studentList[i].style.padding = '0';
+                break;
+
+            case ( '' ):
+                studentList[i].style.display = 'list-item';
+                studentList[i].style.borderBottom = '1px solid #eaeaea';
+                studentList[i].style.margin = '0 0 20px 0';
+                studentList[i].style.padding = '0 0 20px 0';
+                break;
+
+            default:
+                studentList[i].style.display = 'none';
+        }
+    }
+})
+
 /**
  * Creates an element with the given parameters and returns it in HTML format.
  * @param {string} param.element - HTML Element
