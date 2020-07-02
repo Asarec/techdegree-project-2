34f1ed3ff9f7438ec35b868d1a42ce0a24a6956c
Alfredo Garcia <Asarec@users.noreply.github.com>
2020-07-02 18:29:21 -0400

    Updated search event to better reflect the aesthetics of the page.

--- js/script.js
+++ js/script.js
@@ -18,14 +18,22 @@ studentSearchForm.appendChild(searchButton);
  */
 studentSearchForm.addEventListener ('keyup', () => {
     const studentNames = document.querySelectorAll('.student-details');
+    const successfulMatches = [];
 
+    // Cycle through each student and compare key input with indexes of student string.
     studentNames.forEach( student => {
         if ( student.children[1].textContent.indexOf( inputSearch.value ) > -1 ) {
-            student.parentNode.style.display = 'list-item';
+            student.parentNode.setAttribute('style', 'display: list-item; border-bottom: 1px solid #eaeaea');
+
+            // Push successful matches to an array.
+            successfulMatches.push(student);
         } else {
-            student.parentNode.style.display = 'none';
+            student.parentNode.setAttribute('style', 'display: none');
         }
     });
+
+    // Style last list item.
+    successfulMatches[successfulMatches.length - 1].parentNode.setAttribute('style', 'display: list-item; border: none; margin: 0; padding: 0');
 })
 
 /**
